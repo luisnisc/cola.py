398182c10d4dbf2fa0235809fe41fd3dd6c99c69
rsancho64 <rsancho@iesrioarba.es>
2023-11-07 14:15:06 +0100

    .

---
+++ README.md
@@ -0,0 +1,26 @@
+## some ...
+
+[**C++**](https://www.educative.io/answers/stack-implementation-in-cpp)
+
+[**C**](https://www.digitalocean.com/community/tutorials/stack-in-c)
+
+## pseudocodigos para una pila
+
+clase pila:
+
+    nuevaPila(capacidad) -> pila
+    str() -> str
+
+    push(item) -> boolean # success status
+    pop() -> item / None  # not-success status)
+
+    altura() --> entero
+
+    esVacia() -> bool
+    esLlena() -> bool    
+
+    vaciar()
+
+## todo
+
+- [ ] que `pop()` y `vaciar()` limpien también fisicamente
--- pila.py
+++ pila.py
@@ -1,50 +1,63 @@
 class myPila:
 
-    def __init__(self, tamMax):
-        self.tamMax = tamMax
-        self.data =[]
+    def __init__(self, capacidad) -> None:
+        self.capacidad = capacidad
+        self.altura = 0
+        self.buffer = [None] * capacidad
 
-    def push(algo):
-        pass  # TODO **********
+    def __str__(self) -> str:
+        return f"{self.buffer.__str__()}"
 
-    def pop(): # -> algo
-        pass # TODO ************
+    def push(self, item) -> bool:
+        if (self.altura == self.capacidad):
+            return False
+        self.buffer[self.altura] = item
+        self.altura += 1
+        return True
 
+    def pop(self):
+        if (self.altura == 0):
+            return None
+        answ = self.buffer[self.altura-1]
+        self.altura -= 1
+        return answ
+
+    def altura(self) -> int:
+        return self.altura
+
+    def esVacia(self) -> bool:
+        return self.altura != 0
+
+    def esLlena(self) -> bool:
+        return self.altura == self.capacidad
+
+    def vaciar(self):
+        self.altura = 0
 
 if __name__ == "__main__":
 
     p = myPila(3)
-    p.push(11)
-    print(p.data)    
+    print(f"{p}: {p.altura}")
 
-    p.push(22)
-    print(p.data)    
+    if p.push(11):
+        print("push ok")
+    print(f"{p}: {p.altura}")
 
-    p.push(33)
-    print(p.data)    
+    if p.push(22):
+        print("push ok")
+    print(f"{p}: {p.altura}")
 
-    # p.push(44)   # CRACK
-    print(p.data)    
+    if p.push(33):
+        print("push ok")
+    print(f"{p}: {p.altura}")
 
-    print(p.pop())  # -> 33
-    print(p.pop())  # -> 22    
-    print(p.pop())  # -> 11
-    # print(p.pop())  # -> CRAK , None "vacia"
+    if p.push(44):
+        print("push ok")
+    print(f"{p}: {p.altura}")
 
+    item = p.pop()
+    print(item)
+    print(f"{p}: {p.altura}")
 
-
-
-
-
-
-pila.append(22)
-print(pila)     # [11,22]
-
-pila.append(33)
-print(pila)     # [11,22,33]
-
-pila.append(44)
-print(pila)     # [11,22,33,44]
-
-pila.append(55)
-print(pila)     # [11,22,33,44]
+    p.vaciar()
+    print(f"{p}: {p.altura}")
